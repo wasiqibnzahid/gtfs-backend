59aeef4d644115972ff33a28c54543f2802d9cf1
Wasiq Bin Zahid <wasiq.bin.zahid@teraception.com>
2024-02-07 10:01:52 -0800

    added error handling

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -2,7 +2,13 @@ import axios from "axios";
 import { Handler } from "express";
 export function getUrlData(url: string): Handler {
   return async (_req, res) => {
-    const data = await axios.get(url).then((res) => res.data);
-    res.send(data);
+    try {
+
+      const data = await axios.get(url).then((res) => res.data);
+      res.send(data);
+    }
+    catch(e) {
+      res.status(500).send()
+    }
   };
 }
--- src/utils/get-transit.ts
+++ src/utils/get-transit.ts
@@ -4,48 +4,56 @@ import fs from "fs/promises";
 import { createWriteStream } from "fs";
 
 export const getTransit = async () => {
-  const response = await axios({
-    method: "GET",
-    url: process.env.GTFS_STATIC_URL,
-    responseType: "stream",
-    onDownloadProgress: (progress) => {
-      console.log(
-        "Downloading file, done: ",
-        (progress.progress * 100).toFixed(2),
-        " percent at rate: ",
-        progress.rate / (1024 * 1024),
-        " MB/s, loaded: ",
-        progress.loaded / (1024 * 1024),
-        "MBs total : ",
-        progress.total / (1024 * 1024),
-        " MBs"
-      );
-    },
-  });
-
-  const tempPath = path.join(__dirname, "../../static/google_transit_temp.zip");
-  const actualPath = path.join(__dirname, "../../static/google_transit.zip");
-
   try {
-    await fs.unlink(tempPath);
-  } catch (e) {}
-
-  const writer = createWriteStream(tempPath);
-
-  response.data.pipe(writer);
-
-  return new Promise((resolve, reject) => {
-    writer.on("finish", async () => {
-      try {
-        await fs.unlink(actualPath);
-      } catch (e) {}
-      await fs.rename(tempPath, actualPath);
-      setTimeout(getTransit, 1000 * 60 * 60 * 6);
-      resolve(null);
+    const response = await axios({
+      method: "GET",
+      url: process.env.GTFS_STATIC_URL,
+      responseType: "stream",
+      onDownloadProgress: (progress) => {
+        console.log(
+          "Downloading file, done: ",
+          (progress.progress * 100).toFixed(2),
+          " percent at rate: ",
+          progress.rate / (1024 * 1024),
+          " MB/s, loaded: ",
+          progress.loaded / (1024 * 1024),
+          "MBs total : ",
+          progress.total / (1024 * 1024),
+          " MBs"
+        );
+      },
     });
-    writer.on("error", () => {
-      setTimeout(getTransit, 1000 * 60 * 5);
-      reject();
+
+    const tempPath = path.join(
+      __dirname,
+      "../../static/google_transit_temp.zip"
+    );
+    const actualPath = path.join(__dirname, "../../static/google_transit.zip");
+
+    try {
+      await fs.unlink(tempPath);
+    } catch (e) {}
+
+    const writer = createWriteStream(tempPath);
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on("finish", async () => {
+        try {
+          await fs.unlink(actualPath);
+        } catch (e) {}
+        await fs.rename(tempPath, actualPath);
+        setTimeout(getTransit, 1000 * 60 * 60 * 6);
+        resolve(null);
+      });
+      writer.on("error", () => {
+        setTimeout(getTransit, 1000 * 60 * 5);
+        reject();
+      });
     });
-  });
+  } catch (e) {
+    console.error("FAILED to load file", e)
+    setImmediate(getTransit);
+  }
 };
